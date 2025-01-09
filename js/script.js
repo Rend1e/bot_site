e4374d9b26942a506821e4f8498e2b2223d6eeb5
Sergey E <150759718+Rend1e@users.noreply.github.com>
2025-01-09 18:50:19 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let tg = window.Telegram.WebApp;
+let buy = document.getElementById("buy");
+let get_reg = document.getElementById("get_reg");
+        
+buy.addEventListener("click", () => {
+  document.getElementById("main").style.display = "none";
+  document.getElementById("popup").style.display = "block"
+  document.getElementById("reg").style.display = "block";
+  document.getElementById("user_name").value = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
+})        
+
+get_reg.addEventListener("click", () => {
+  document.getElementById("error").innerText = ' ';
+  let name = document.getElementById("user_name").value;
+  let email = document.getElementById("user_email").value;
+  let phone = document.getElementById("user_phone").value;
+
+if (name.length < 2) { 
+  document.getElementById("error").innerText = 'Ошибка в имени';
+  return;
+}
+  if (email.length < 5) { 
+    document.getElementById("error").innerText = 'Ошибка в email';
+    return;
+}
+if (phone.length < 10) { 
+  document.getElementById("error").innerText = 'Ошибка в номере телефона';
+  return;
+}
+
+let data = {
+  name: name,
+  email: email,
+  phone: phone
+}
+tg.sendData(JSON.stringify(data));
+tg.close();
+})
+
